c4f2e518b66bfae4e5180269c82157698ee467a8
Mendenga <mendenga.takougnadi@hotmail.com>
2024-05-27 11:24:34 +0200

    last commit?

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -15,9 +15,5 @@ export default defineNuxtConfig({
     cloudinaryCloudName: process.env.CLOUDINARY_CLOUD_NAME,
     cloudinaryApiKey: process.env.CLOUDINARY_API_KEY,
     cloudinaryApiSecret: process.env.CLOUDINARY_API_SECRET,
-  },
-
-  devtools: {
-    enabled: false
   }
 })
